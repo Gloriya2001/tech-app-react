cdeff200b79d5cff969dd20e35bbb95208721bc9
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-13 03:12:59 +0530

    input read for search done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+
+
+const [data,searchData] = useState(
+
+   {"event":""} 
+
+)
+
+const inputHandler = (event) => {
+searchData({...data,[event.target.name]:event.target.value})
+}
+
+const readValue = () =>{
+    console.log(data)
+}
+
+
     return (
         <div>
 
@@ -9,7 +26,7 @@ const Search = () => {
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <h2>Search Students in the Event</h2>
-                        
+
                         <div className="row g-3">
 
 
@@ -17,12 +34,12 @@ const Search = () => {
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Event</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='event' value={data.event} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                                <button className="btn btn-success">Search</button>
+                                <button className="btn btn-success" onClick={readValue}>Search</button>
 
                             </div>
 
