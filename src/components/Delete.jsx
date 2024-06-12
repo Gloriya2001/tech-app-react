3e63ee2d10ce5b153381c3130dbe3554393be040
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-13 05:02:06 +0530

    dynamic done on delete

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,21 +1,62 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 const Delete = () => {
 
-const [data,setData] = useState(
-{
-    "name":""
-}
-)
+    const [data, setData] = useState(
+        {
+            "name": ""
+        }
+        
+    )
+
+    const [result, setResult] = useState([])
 
 
-const inputHandler = (event) => {
-    setData({...data,[event.target.name]:event.target.value})
-}
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
 
-const readValue = () => {
-    console.log(data)
-}
+
+    
+
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/search", data).then(
+
+            (response) => {
+                setResult(response.data)
+            }
+
+
+        ).catch(
+
+            (error) => {
+                console.log(error.message)
+                alert("error occured")
+            }
+        )
+    }
+
+    const deletesample = (id) => {
+        let input = { "_id": id }
+        axios.post("http://localhost:8080/delete", input).then(
+
+            (response) => {
+
+                console.log(response.data)
+                if (response.data.status == "success") {
+                    alert("Successfully Deleted")
+
+                } else {
+                    alert("Error!")
+                }
+
+            }
+
+        ).catch()
+    }
+
 
 
     return (
@@ -34,14 +75,60 @@ const readValue = () => {
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Student Name</label>
-                                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
+                                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
 
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-Delete" onClick={readValue}>Delete</button>
+
+
+
+                                <button className="btn btn-danger" onClick={readValue}>Delete</button>
+
+
 
                             </div>
+
+
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <table class="table table stripped">
+
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">No.</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">course</th>
+                                            <th scope="col">Event</th>
+                                            <th scope="col">group</th>
+                                        </tr>
+                                    </thead>
+
+
+                                    <tbody>
+
+
+                                        {result.map(
+                                            (value, index) => {
+                                                return <tr>
+                                                    <th>{index + 1}</th>
+                                                    <td>{value.name}</td>
+                                                    <td>{value.course}</td>
+                                                    <td>{value.event}</td>
+                                                    <td>{value.gpname}</td>
+                                                    <td> <button className="btn btn-danger" onClick={()=>{ deletesample(value._id) }}>Delete</button></td>
+                                                </tr>
+                                            }
+                                        )}
+
+
+                                    </tbody>
+
+
+                                </table>
+
+                            </div>
+
 
                         </div>
 
